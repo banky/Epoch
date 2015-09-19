66397eb602474160a5782602c2a68fa6c821fb17
michael-jonas <michael.jonas44@gmail.com>
2015-09-19 10:18:25 -0400

    Styling landing page

--- server/mongodb.js
+++ server/mongodb.js
@@ -18,7 +18,31 @@
 				}
 			}); 
 		}
-	});
+
+		exports.addLocations = function(req,res) {
+			Locations.findOne({'id': req.user._id}, function(err,user){
+				if(err){
+					console.log('Error finding user in addLocations:' + err);
+					res.status(400).send();
+				}
+
+				else{
+					locations.latitude.push(req.body.latitude);
+					locations.longitude.push(req.body.longitude);
+					locations.radius.push(req.body.radius);
+					locations.save(function(err,user){
+						if(err){
+							console.log('An error occured while adding the new location:' + err);
+
+						}
+
+						else{
+							res.status(200);
+						}
+					});
+				}
+			});
+		};
 	mongoose.connect('mongodb://localhost/mydb');
 
 }());
