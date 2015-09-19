7edc85444ee40a662e1447cba64d2b16476d484b
bankya <bankiadebajo@hotmail.com>
2015-09-19 14:34:54 -0400

    Add config to heroku

---
+++ server/config.js
@@ -0,0 +1,9 @@
+(function () {
+	'use strict';
+
+	var apiKey = 'F415EF21E7B64498BACA';
+	var apiSecret = '309B167F924148BFAB7FF477910F96D9';
+
+	exports.apiKey = apiKey;
+	exports.apiSecret = apiSecret;
+}());
