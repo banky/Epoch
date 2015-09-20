709094927631da2882a5ce79b2320cff6a2947c1
bankya <bankiadebajo@hotmail.com>
2015-09-20 08:13:04 -0400

    Remove android

--- android/HackTheNorth/app/src/main/java/example/com/server/ServerConstants.java
+++
@@ -1,8 +0,0 @@
-package example.com.server;
-
-/**
- * Created by ianlo on 2015-09-19.
- */
-public class ServerConstants {
-    public static final String SERVER = "https://frozen-eyrie-7100.herokuapp.com";
-}
